db0a5b56effbeb0040672f9209511563c8a7b351
Ratna Katuwal <parbesh456@gmail.com>
2024-12-19 16:44:51 +0545

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation ("com.intuit.ssp:ssp-android:1.1.1")
     implementation ("com.intuit.sdp:sdp-android:1.1.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
---
+++ app/src/main/java/com/example/novelnestadmin/UserDetails.java
@@ -0,0 +1,20 @@
+package com.example.novelnestadmin;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UserDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_details);
+
+    }
+}
